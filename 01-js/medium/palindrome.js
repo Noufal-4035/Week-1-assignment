642b9d8d8d620356aaa790f540bc82ab757471b4
Noufal-4035 <140302131+Noufal-4035@users.noreply.github.com>
2023-11-04 15:06:53 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,17 @@
 */
 
 function isPalindrome(str) {
-  return true;
+
+  for(let i = 0; i < str.length/2; i++)
+    {
+      if(str[i]!=str[str.length-1-i])
+      {
+        return false;
+      }
+    }
+
+  return true;  
+
 }
 
 module.exports = isPalindrome;
